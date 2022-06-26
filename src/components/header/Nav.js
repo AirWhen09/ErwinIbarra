db0880d146d7cc5b765f2dc4928521ac816ca252
Erwin <ibarra.erwin09@gmail.com>
2022-06-26 14:46:32 +0800

    update contact links

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -9,10 +9,10 @@ const Nav = () => {
           <p ><span className='text-default'>Air</span><i>When</i></p>
       </Link>
     <ul>
-        <li className="social"><a href="https://web.facebook.com/ibarra.erwin.9/"><i className="bi bi-facebook"></i></a></li>
-        <li className="social"><a href="https://web.facebook.com/ibarra.erwin.9/"><i className="bi bi-envelope"></i></a></li>
-        <li className="social"><a href="https://web.facebook.com/ibarra.erwin.9/"><i className="bi bi-discord"></i></a></li>
-        <li className="social"><a href="https://github.com/AirWhen09"><i className="bi bi-github"></i></a></li>
+        <li className="social"><a href="https://web.facebook.com/ibarra.erwin.9/" title='Facebook Account'><i className="bi bi-facebook"></i></a></li>
+        <li className="social"><a href="https://mail.google.com/mail/u/0/#search/ibarra.erwin09@gmail.com" title='ibarra.erwin09@gmail.com'><i className="bi bi-envelope"></i></a></li>
+        <li className="social"><a href="https://discord.gg/DpmkmNTe" title='Discord Server'><i className="bi bi-discord"></i></a></li>
+        <li className="social"><a href="https://github.com/AirWhen09" title='Github Repository'><i className="bi bi-github"></i></a></li>
         <li><a href="https://web.facebook.com/ibarra.erwin.9/" className="btn btn-primary">Download CV</a></li>
     </ul>
   </header>
